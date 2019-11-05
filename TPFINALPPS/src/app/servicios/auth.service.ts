9353c91ce3db6862932db213c58be8ea96c48f40
Juani <nacho.mone@gmail.com>
2019-11-05 11:23:27 -0300

    Servicios de autenticacion terminados con async y await

--- TPFINALPPS/src/app/servicios/auth.service.ts
+++ TPFINALPPS/src/app/servicios/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
 import { UsuarioService } from './usuario.service';
 import { Usuario } from '../clases/usuario';
 import { Router } from '@angular/router';
@@ -9,67 +8,64 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
-  userData: Observable<firebase.User>;
-
   constructor(
     private angularFireAuth: AngularFireAuth,
     private userService: UsuarioService,
     private router: Router
-  ) {
-    this.userData = angularFireAuth.authState;
-  }
+  ) {}
 
-  registracion(usuario: Usuario, password: string): string {
+  async registracion(usuario: Usuario, password: string): Promise<string> {
     let salida = 'Se dio de alta un nuevo usuario';
 
-    this.angularFireAuth.auth
-      .createUserWithEmailAndPassword(usuario.email, password)
-      .then(res => {
-        this.userService.persistirUsuario(usuario, res.user.uid);
-      })
-      .catch(error => {
-        salida = error.message;
-      });
+    try {
+      const res = await this.angularFireAuth.auth.createUserWithEmailAndPassword(
+        usuario.email,
+        password
+      );
+      this.userService.persistirUsuario(usuario, res.user.uid);
+    } catch (err) {
+      salida = err.message;
+    }
 
     return salida;
   }
 
-  ingresar(email: string, password: string): string {
+  async ingresar(email: string, password: string): Promise<string> {
     let salida = 'El usuario ingreso correctamente';
 
-    this.angularFireAuth.auth
-      .signInWithEmailAndPassword(email, password)
-      .then(res => {})
-      .catch(err => {
-        salida = err.message;
-      });
+    try {
+      const res = await this.angularFireAuth.auth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+    } catch (err) {
+      salida = err.message;
+    }
 
     return salida;
   }
 
-  ingresarAnonimo(): string {
+  async ingresarAnonimo(): Promise<string> {
     let salida = 'El usuario anonimo ingreso correctamente';
 
-    this.angularFireAuth.auth
-      .signInAnonymously()
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        salida = err.message;
-      });
+    try {
+      const res = await this.angularFireAuth.auth.signInAnonymously();
+    } catch (err) {
+      salida = err.message;
+    }
 
     return salida;
   }
 
-  salir(): string {
+  async salir(): Promise<string> {
     let salida = 'El usuario salio correctamente';
-    this.angularFireAuth.auth.signOut().then((res) => {
-      console.log(res);
+
+    try {
+      const res = await this.angularFireAuth.auth.signOut();
       this.router.navigate(['/login']);
-    }).catch(err => {
+    } catch (err) {
       salida = err.message;
-    });
+    }
 
     return salida;
   }
--- TPFINALPPS/src/app/servicios/usuario.service.ts
+++ TPFINALPPS/src/app/servicios/usuario.service.ts
@@ -1,19 +1,73 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Usuario } from '../clases/usuario';
+import { Cliente } from '../clases/cliente';
+import { Supervisor } from '../clases/supervisor';
+import { Empleado } from '../clases/empleado';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  constructor(
-    private firestore: AngularFirestore
-  ) {}
+  constructor(private firestore: AngularFirestore) {}
 
   persistirUsuario(usuario: Usuario, uid: string) {
     this.firestore
       .collection('/usuarios')
       .doc(uid)
       .set(Object.assign({}, JSON.parse(JSON.stringify(usuario))));
+  }
+
+  async traerUsuario(uid: string): Promise<Usuario> {
+    let salida = null;
+
+    try {
+      const doc = await this.firestore
+        .collection('/usuarios')
+        .doc(uid)
+        .ref.get();
+      const datos = JSON.parse(JSON.stringify(doc.data()));
+      switch (datos.rol) {
+        case 'cliente':
+          salida = new Cliente(
+            datos.nombre,
+            datos.apellido,
+            datos.dni,
+            datos.foto,
+            datos.activo,
+            datos.email,
+            datos.rol
+          );
+          break;
+        case 'supervisor':
+          salida = new Supervisor(
+            datos.nombre,
+            datos.apellido,
+            datos.dni,
+            datos.foto,
+            datos.activo,
+            datos.email,
+            datos.rol,
+            datos.cuil,
+            datos.perfil
+          );
+          break;
+        case 'empleado':
+          salida = new Empleado(
+            datos.nombre,
+            datos.apellido,
+            datos.dni,
+            datos.foto,
+            datos.activo,
+            datos.email,
+            datos.rol,
+            datos.cuil,
+            datos.tipoEmpleado
+          );
+          break;
+      }
+    } catch (err) {}
+
+    return salida;
   }
 }
